18828c436c8494eedb50974c48837147e45d58a6
pooniaMayank <pooniamayank8@gmail.com>
2024-12-18 20:57:05 +0530

    final

--- backend/server.js
+++ backend/server.js
@@ -42,10 +42,7 @@ if (!fs.existsSync(uploadsDir)) {
 }
 // MongoDB Connection
 mongoose
-  .connect(
-    process.env.MONGO_URI ||
-      "mongodb+srv://admin:OvpIVRbKRSH92ZQW@cluster0.hymysuv.mongodb.net/dtu-research-portal"
-  )
+  .connect("mongodb://localhost:27017/dtu-research-portal")
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
