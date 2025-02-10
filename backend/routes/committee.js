6085b935662f21a903281aeff9d86e2f54df2029
Talha Ansari <talhaansari1606@gmail.com>
2025-02-10 10:40:01 +0530

    update v3

--- backend/routes/committee.js
+++ backend/routes/committee.js
@@ -30,25 +30,33 @@ router.get('/research-papers', authorizeCommitteeMember, async (req, res) => {
   router.put('/research-papers/:paperId/status', authorizeCommitteeMember, async (req, res) => {
     const { paperId } = req.params;
     const { status, comments } = req.body; // status can be 'approved' or 'rejected'
-  
+    console.log(status);
     try {
       const paper = await ResearchPaper.findById(paperId);
       if (!paper) return res.status(404).json({ error: 'Research paper not found' });
+      console.log(paper);
       
       paper.status = status; // Add a `status` field in the ResearchPaper schema
       if(status === 'approved'){
         paper.approvedBy = req.user.id;
+        paper.status = 'approved';
       }else if(status === 'suspended'){
         paper.suspendedBy = req.user.id;
+        paper.status = 'suspended';
       }
       else if(status === 'underReview'){
         paper.reviewedBy = req.user.id;
+        paper.status = 'underReview';
       }
       else if(status === 'rejected'){
         paper.rejectedBy = req.user.id;
+        paper.status = 'rejected';
       }
 
-      paper.comments = comments || null; // Add a `comments` field in the schema if needed
+      
+
+
+      // paper.comments = comments || null; // Add a `comments` field in the schema if needed
       await paper.save();
   
       res.status(200).json({ message: `Research paper ${status}` });
